e5df5540ffc9dde08ecabe935c16709738d2d783
marcivan29 <marcivan05@gmail.com>
2025-07-27 21:54:20 -0400

    Add files via upload

--- Utils/DbConnection.py
+++ Utils/DbConnection.py
@@ -27,3 +27,30 @@ def update_balance(user_id, new_balance):
         
     except:
         return False
+    
+def create_user(name):
+    try:
+        conn = sqlite3.connect('Databases.db')
+        cursor = conn.cursor()
+        update_query = SqlQuery.add_user_query()
+        cursor.execute(update_query, (name, 0))
+        conn.commit()
+        conn.close()
+        
+    except:
+        return False
+
+def check_existing_user(name):
+
+    conn = sqlite3.connect('Databases.db')
+    cursor = conn.cursor()
+    cursor.execute(SqlQuery.check_existing_user(), (name,))
+    rows = cursor.fetchall()
+    for row in rows:
+        
+        if row == '':
+            return False
+        else: 
+            return True
+    conn.close()
+    
--- Utils/SqlQuery.py
+++ Utils/SqlQuery.py
@@ -3,3 +3,9 @@ def balance_inquiry():
 
 def cashin_debit_query():
     return """UPDATE DigitalWallet SET Balance = ? WHERE Id = ?;"""
+
+def add_user_query():
+    return '''INSERT INTO DigitalWallet (Name, Balance) VALUES (?, ?)'''
+
+def check_existing_user():
+    return "SELECT Name FROM DigitalWallet WHERE Name = ?"
